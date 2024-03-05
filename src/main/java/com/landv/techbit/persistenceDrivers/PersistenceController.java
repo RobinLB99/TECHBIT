dc3f02a92e9a7437e6c0d9180612fb6601921691
RobinLB99 <rjlb991102@hotmail.com>
2024-03-05 15:56:26 -0500

    Modify persistence driver

--- src/main/java/com/landv/techbit/persistenceDrivers/PersistenceController.java
+++ src/main/java/com/landv/techbit/persistenceDrivers/PersistenceController.java
@@ -8,12 +8,7 @@ import javax.persistence.Persistence;
  * @author Robin
  */
 public class PersistenceController {
-    
-    
+
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.landv_TechBit_war_1.0PU");
-    
-    ClientePersonaJpaController pclienteJpa = new ClientePersonaJpaController(emf);
-    EmpleadoJpaController empleadoJpa = new EmpleadoJpaController(emf);
-    ClienteEmpresaJpaController eclienteJpa = new ClienteEmpresaJpaController(emf);
-    
+
 }
