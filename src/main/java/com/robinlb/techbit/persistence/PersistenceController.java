1042fe1511d7a9c5b898d717fa7db0597757f180
RobinLB99 <rjlb991102@hotmail.com>
2024-03-08 09:04:31 -0500

    ADD persistence methods at class "Empleado"

--- src/main/java/com/robinlb/techbit/persistence/PersistenceController.java
+++ src/main/java/com/robinlb/techbit/persistence/PersistenceController.java
@@ -1,5 +1,10 @@
 package com.robinlb.techbit.persistence;
 
+import com.robinlb.techbit.model.Empleado;
+import com.robinlb.techbit.persistence.exceptions.NonexistentEntityException;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -9,6 +14,41 @@ import javax.persistence.Persistence;
  */
 public class PersistenceController {
 
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.landv_TechBit_war_1.0PU");
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.robinlb_TechBit_war_1.0PU");
+
+    EmpleadoJpaController empleadoJpa = new EmpleadoJpaController(emf);
+    UsuarioJpaController usuarioJpa = new UsuarioJpaController(emf);
+    
+    /**
+     * 
+     * @param empleado 
+     */
+    public void createEmploy(Empleado empleado) {
+        empleadoJpa.create(empleado);
+    }
+    
+    public void destroyEmploy(Long id) {
+        try {
+            empleadoJpa.destroy(id);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(PersistenceController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void updateEmploy(Empleado empleado) {
+        try {
+            empleadoJpa.edit(empleado);
+        } catch (Exception ex) {
+            Logger.getLogger(PersistenceController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public Empleado viewEmploy(Long id) {
+        return empleadoJpa.findEmpleado(id);
+    }
+    
+    public Collection<Empleado> employList() {
+        return empleadoJpa.findEmpleadoEntities();
+    }
 
 }
