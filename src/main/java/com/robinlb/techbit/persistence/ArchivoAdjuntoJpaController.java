7a3d17c9d01b7b70539ae763655fd615d67a1dd6
RobinLB99 <rjlb991102@hotmail.com>
2024-03-08 08:56:23 -0500

    CHANGE import dependencies

--- src/main/java/com/robinlb/techbit/persistence/ArchivoAdjuntoJpaController.java
+++ src/main/java/com/robinlb/techbit/persistence/ArchivoAdjuntoJpaController.java
@@ -1,18 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.robinlb.techbit.persistence;
 
 import com.robinlb.techbit.model.ArchivoAdjunto;
 import com.robinlb.techbit.persistence.exceptions.NonexistentEntityException;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import java.io.Serializable;
-import jakarta.persistence.Query;
-import jakarta.persistence.EntityNotFoundException;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 /**
