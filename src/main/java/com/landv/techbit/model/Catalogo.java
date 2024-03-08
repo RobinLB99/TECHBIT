b38f9d692b0a5a37b3ac8e72a8a0dadd728af587
RobinLB99 <rjlb991102@hotmail.com>
2024-03-07 21:34:02 -0500

    CREATE class "Catalogo"

---
+++ src/main/java/com/landv/techbit/model/Catalogo.java
@@ -0,0 +1,131 @@
+package com.landv.techbit.model;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Robin
+ */
+@Entity
+@Table(name = "catalogo")
+@NamedQueries({
+    @NamedQuery(name = "Catalogo.findAll", query = "SELECT c FROM Catalogo c")})
+public class Catalogo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "catalogo_id")
+    private Long catalogoId;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "tipo")
+    private int tipo;
+
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "precio")
+    private double precio;
+
+    public Catalogo() {
+    }
+
+    public Catalogo(Long catalogoId, String nombre, String descripcion, int tipo, double precio) {
+        this.catalogoId = catalogoId;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.tipo = tipo;
+        this.precio = precio;
+    }
+
+    public Long getCatalogoId() {
+        return catalogoId;
+    }
+
+    public void setCatalogoId(Long catalogoId) {
+        this.catalogoId = catalogoId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (catalogoId != null ? catalogoId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Catalogo)) {
+            return false;
+        }
+        Catalogo other = (Catalogo) object;
+        if ((this.catalogoId == null && other.catalogoId != null) || (this.catalogoId != null && !this.catalogoId.equals(other.catalogoId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Catalogo{" + "catalogoId=" + catalogoId + ", nombre=" + nombre + ", descripcion=" + descripcion + ", tipo=" + tipo + ", precio=" + precio + '}';
+    }
+
+}
