084b62f7cfa6e99cff03356344078e6bbb3440c3
RobinLB99 <rjlb991102@hotmail.com>
2024-03-08 14:00:06 -0500

    ADD logic methods at classes Empleado and Usuario

--- src/main/java/com/robinlb/techbit/controllers/LogicController.java
+++ src/main/java/com/robinlb/techbit/controllers/LogicController.java
@@ -1,6 +1,9 @@
 package com.robinlb.techbit.controllers;
 
+import com.robinlb.techbit.model.Empleado;
+import com.robinlb.techbit.model.Usuario;
 import com.robinlb.techbit.persistence.PersistenceController;
+import java.util.Collection;
 
 /**
  *
@@ -9,5 +12,53 @@ import com.robinlb.techbit.persistence.PersistenceController;
 public class LogicController {
     
     PersistenceController perControl = new PersistenceController();
+    
+    /**
+     * 
+     * @param empleado 
+     */
+    public void crearEmpleado(Empleado empleado) {
+        perControl.createEmploy(empleado);
+    }
+    
+    public void eliminarEmpleado(Long id) {
+        perControl.destroyEmploy(id);
+    }
+    
+    public void actualizarEmpleado(Empleado empleado) {
+        perControl.updateEmploy(empleado);
+    }
+    
+    public Empleado verEmpleado(Long id) {
+        return perControl.viewEmploy(id);
+    }
+    
+    public Collection<Empleado> listaEmpleados() {
+        return perControl.employList();
+    }
+    
+    /**
+     * 
+     * @param usuario 
+     */
+    public void crearUsuario(Usuario usuario) {
+        perControl.createUser(usuario);
+    }
+    
+    public void eliminarUsuario(Long id) {
+        perControl.destroyUsuario(id);
+    }
+    
+    public void actualizarUsuario(Usuario usuario) {
+        perControl.updateUser(usuario);
+    }
+    
+    public Usuario verUsuario(Long id) {
+        return perControl.viewUser(id);
+    }
+    
+    public Collection<Usuario> listaUsuarios() {
+        return perControl.usersList();
+    }
     
 }
