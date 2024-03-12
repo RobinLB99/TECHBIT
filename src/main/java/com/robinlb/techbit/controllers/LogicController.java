0106d080d53e5a8999c6423e7c32314e89839d66
RobinLB99 <rjlb991102@hotmail.com>
2024-03-12 14:32:19 -0500

    Nuevo metodo logico para la busqueda de usuario por nombre de usuario

--- src/main/java/com/robinlb/techbit/controllers/LogicController.java
+++ src/main/java/com/robinlb/techbit/controllers/LogicController.java
@@ -10,55 +10,68 @@ import java.util.Collection;
  * @author Robin
  */
 public class LogicController {
-    
+
     PersistenceController perControl = new PersistenceController();
-    
+
     /**
-     * 
-     * @param empleado 
+     *
+     * @param empleado
      */
     public void crearEmpleado(Empleado empleado) {
         perControl.createEmploy(empleado);
     }
-    
+
     public void eliminarEmpleado(Long id) {
         perControl.destroyEmploy(id);
     }
-    
+
     public void actualizarEmpleado(Empleado empleado) {
         perControl.updateEmploy(empleado);
     }
-    
+
     public Empleado verEmpleado(Long id) {
         return perControl.viewEmploy(id);
     }
-    
+
     public Collection<Empleado> listaEmpleados() {
         return perControl.employList();
     }
-    
+
     /**
-     * 
-     * @param usuario 
+     *
+     * @param usuario
      */
     public void crearUsuario(Usuario usuario) {
         perControl.createUser(usuario);
     }
-    
+
     public void eliminarUsuario(Long id) {
         perControl.destroyUsuario(id);
     }
-    
+
     public void actualizarUsuario(Usuario usuario) {
         perControl.updateUser(usuario);
     }
-    
+
     public Usuario verUsuario(Long id) {
         return perControl.viewUser(id);
     }
-    
+
+    public Usuario verUsuarioPorNombreUsuario(String nombreUsuario) {
+        Collection<Usuario> usuarios = perControl.usersList();
+        Usuario usuario = null;
+        
+        for (Usuario u : usuarios) {
+            if (u.getNombreUsuario().equals(nombreUsuario)) {
+                usuario = u;
+                break;
+            }
+        }
+        return usuario;
+    }
+
     public Collection<Usuario> listaUsuarios() {
         return perControl.usersList();
     }
-    
+
 }
