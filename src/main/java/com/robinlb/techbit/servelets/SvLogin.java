4f92a69ead03f1b7d11de0c7303facbcfdb39df4
RobinLB99 <rjlb991102@hotmail.com>
2024-03-12 14:35:10 -0500

    Creación de Servelet para el inicio de sessión

---
+++ src/main/java/com/robinlb/techbit/servelets/SvLogin.java
@@ -0,0 +1,65 @@
+package com.robinlb.techbit.servelets;
+
+import com.robinlb.techbit.controllers.LogicController;
+import com.robinlb.techbit.controllers.PasswordHashed;
+import com.robinlb.techbit.model.Usuario;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Robin
+ */
+@WebServlet(name = "SvLogin", urlPatterns = {"/SvLogin"})
+public class SvLogin extends HttpServlet {
+
+    LogicController control = new LogicController();
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+
+        String username = (String) request.getParameter("nombre_usuario");
+        String password = (String) request.getParameter("contraseña");
+
+        try {
+            Usuario usuario = control.verUsuarioPorNombreUsuario(username);
+            PasswordHashed passwordhashed = new PasswordHashed();
+            boolean passwordMatched = passwordhashed.match(password, usuario.getContraseña());
+
+            if (passwordMatched) {
+                response.sendRedirect("dashboard.jsp");
+            } else {
+                response.sendRedirect("page-login.jsp?password=incorrect");
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            response.sendRedirect("page-login.jsp?user=false");
+        }
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
