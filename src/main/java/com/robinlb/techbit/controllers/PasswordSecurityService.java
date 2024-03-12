ce860834490a5d35a65793d87ca31541da2b638e
RobinLB99 <rjlb991102@hotmail.com>
2024-03-12 18:18:07 -0500

    Comentarios

--- src/main/java/com/robinlb/techbit/controllers/PasswordSecurityService.java
+++ src/main/java/com/robinlb/techbit/controllers/PasswordSecurityService.java
@@ -4,18 +4,17 @@ package com.robinlb.techbit.controllers;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
- *
- * @author Robin
- *
+ * Proporciona servicio de cifrado y validación de contraseñas.
  */
 public class PasswordSecurityService {
 
     BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     /**
-     *
+     * Cifra la contraseña ingresada.
+     * 
      * @param password
-     * @return contraseña encriptada
+     * @return String passwordEncrypted
      */
     public String encrypt(String password) {
         return encoder.encode(password);
@@ -23,11 +22,12 @@ public class PasswordSecurityService {
     }
 
     /**
-     *
+     * Retorna verdadero o falso según coincida la contraseña ingresada con la
+     * contraseña encriptada almacenada.
+     * 
      * @param password
      * @param hashedPassword
-     * @return true o false según coincida la contraseña ingresada con la
-     * contraseña encriptada almacenada.
+     * @return true / false 
      */
     public boolean matches(String password, String hashedPassword) {
         return encoder.matches(password, hashedPassword);
