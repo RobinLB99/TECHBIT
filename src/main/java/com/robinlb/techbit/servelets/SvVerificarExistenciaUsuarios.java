008895ba850da048e789e362cf5a59e03cbfc3ae
RobinLB99 <rjlb991102@hotmail.com>
2024-03-13 15:03:19 -0500

    Se añade nueva logica de sesiones activas o inactivas.

--- src/main/java/com/robinlb/techbit/servelets/SvVerificarExistenciaUsuarios.java
+++ src/main/java/com/robinlb/techbit/servelets/SvVerificarExistenciaUsuarios.java
@@ -8,26 +8,50 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.util.Collection;
 
 /**
- *  Verifica si existe algún usuario registrado.
- *  Si existe uno o mas usuarios registrados, redirige a la pagina de login.
- *  Si no existe ningun usuario, redirige a la pagina de registro de usuario root.
- *  Se ejecuta al iniciar la aplicación.
+ * Se ejecuta al iniciar la aplicación y verifica si existe algún usuario registrado. 
  */
 @WebServlet(name = "SvVerificarExistenciaUsuarios", urlPatterns = {"/SvVerificarExistenciaUsuarios"})
 public class SvVerificarExistenciaUsuarios extends HttpServlet {
-    
+
     LogicController control = new LogicController();
 
+    /**
+    * Si existe uno o mas usuarios registrados, redirige a la pagina de login.
+    * Si no existe ningún usuario, redirige a la pagina de registro de usuario root.
+    * Si iniciada una sesión, se establece la URI raiz, este servelet redirigira al Dashboard inmediatamente.
+    * Si no existe una sesión activa y se establece la URI raiz, este servelet redirigira a la pagina Login.
+    * 
+    * @param request
+    * @param response
+    * @throws ServletException
+    * @throws IOException 
+    */
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        Collection<Usuario> usuarios = control.listaUsuarios();
+
+        HttpSession requestSession = null;
+        String log = (String) request.getSession().getAttribute("session");
         
+        if (log == null) {
+            log = "logoff";
+            requestSession = request.getSession();
+            requestSession.setAttribute("session", log);
+        }
+
+        Collection<Usuario> usuarios = control.listaUsuarios();
+
         if (usuarios.isEmpty()) {
             response.sendRedirect("RegisterRoot.jsp");
-        } else response.sendRedirect("Login.jsp");
+        } else if (log.equals("logon")) {
+            response.sendRedirect("Dashboard.jsp");
+        } else if (log.equals("logoff")) {
+            response.sendRedirect("Login.jsp");
+        }
     }
 
     @Override
