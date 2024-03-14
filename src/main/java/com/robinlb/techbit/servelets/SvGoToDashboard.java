0509765a26dabe17bfb7ff24420c9c93e532ad0f
RobinLB99 <rjlb991102@hotmail.com>
2024-03-13 20:15:30 -0500

    Servelet para la redirección al dashboard

---
+++ src/main/java/com/robinlb/techbit/servelets/SvGoToDashboard.java
@@ -0,0 +1,40 @@
+package com.robinlb.techbit.servelets;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Robin
+ */
+@WebServlet(name = "SvGoToDashboard", urlPatterns = {"/SvGoToDashboard"})
+public class SvGoToDashboard extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        response.sendRedirect("Dashboard.jsp");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
