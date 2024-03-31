aba497e6cd2f193596d1e3fca009e0cb686420c1
RobinLB99 <rjlb991102@hotmail.com>
2024-03-31 13:29:07 -0500

    Se añade servicio de consulta de lista de empleados

--- src/main/java/com/robinlb/techbit/servelets/SvGoToEmployesList.java
+++ src/main/java/com/robinlb/techbit/servelets/SvGoToEmployesList.java
@@ -1,11 +1,16 @@
 package com.robinlb.techbit.servelets;
 
+import com.robinlb.techbit.controllers.LogicController;
+import com.robinlb.techbit.model.Empleado;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.Collection;
+import java.util.List;
 
 @WebServlet(name = "SvGoToEmployesList", urlPatterns = {"/SvGoToEmployesList"})
 public class SvGoToEmployesList extends HttpServlet {
@@ -19,6 +24,13 @@ public class SvGoToEmployesList extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+        
+        LogicController control = new LogicController();
+        Collection<Empleado> listaEmpleados = control.listaEmpleados();
+        
+        HttpSession es = request.getSession();
+        es.setAttribute("listaEmpleados", listaEmpleados);
+        
         response.sendRedirect("Empleados.jsp");
     }
 
