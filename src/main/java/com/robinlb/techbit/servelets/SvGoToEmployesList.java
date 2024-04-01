e5b2163aa8fda3e62e9447a6a4a2d72c3a020481
RobinLB99 <rjlb991102@hotmail.com>
2024-04-01 14:53:07 -0500

    Se añade la toma de atributos de sesión para la redirección y establecimiento de variables según exista registros eliminados de la tabla empleados.

--- src/main/java/com/robinlb/techbit/servelets/SvGoToEmployesList.java
+++ src/main/java/com/robinlb/techbit/servelets/SvGoToEmployesList.java
@@ -25,15 +25,52 @@ public class SvGoToEmployesList extends HttpServlet {
         processRequest(request, response);
 
         LogicController control = new LogicController();
+        Integer numNotDel = null;
+        Integer lengthArray = null;
 
+        /**
+         * Persiste la lista de empleados desde la base de datos
+         */
         try {
             Collection<Empleado> listaEmpleados = control.listaEmpleados();
 
             HttpSession es = request.getSession();
             es.setAttribute("listaEmpleados", listaEmpleados);
-        
-            response.sendRedirect("Empleados.jsp");
-            
+
+            /**
+             * Toma el numero de registros no eliminados de la tabla empleados
+             * mediante el atributo de sesión "numNoEliminados"
+             */
+            try {
+                numNotDel = Integer.parseInt(
+                    (String) request.getSession().getAttribute("numNoEliminados")
+                );
+
+            } catch (Exception ex) {System.out.println(ex.getMessage());}
+
+            /**
+             * Toma la longitud del la lista de empleados eliminados
+             * mediante el atributo de sesión "arrIDsForDelLength"
+             */
+            try {
+                lengthArray = (int) request.getSession().getAttribute("arrIDsForDelLength");
+
+            } catch (Exception a) {System.out.println(a.getMessage());}
+
+            /**
+             * Redirecciona '/Empleados.jsp' y establece variables según
+             * exista registros eliminados de la tabla empleado
+             */
+            if (numNotDel == null) {
+                response.sendRedirect("Empleados.jsp");
+            } else if (numNotDel == 0) {
+                response.sendRedirect("Empleados.jsp?del=true&notDel=false");
+            } else if (numNotDel > 0) {
+                response.sendRedirect("Empleados.jsp?del=true&notDel=true");
+            } else if (numNotDel == lengthArray) {
+                response.sendRedirect("Empleados.jsp?del=false&notDel=true");
+            }
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
