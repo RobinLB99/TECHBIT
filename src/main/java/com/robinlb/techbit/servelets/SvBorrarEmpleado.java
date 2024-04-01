8097e0427222d4e89f21ceffcb8031d3af3f6189
RobinLB99 <rjlb991102@hotmail.com>
2024-04-01 14:57:21 -0500

    Se crea Servelet para la eliminación de registros de la tabla empleados

---
+++ src/main/java/com/robinlb/techbit/servelets/SvBorrarEmpleado.java
@@ -0,0 +1,60 @@
+package com.robinlb.techbit.servelets;
+
+import com.google.gson.Gson;
+import com.robinlb.techbit.controllers.LogicController;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.List;
+
+@WebServlet(name = "SvBorrarEmpleado", urlPatterns = {"/SvBorrarEmpleado"})
+public class SvBorrarEmpleado extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        LogicController control = new LogicController();
+        Gson gson = new Gson();
+        
+        int numNotDel = 0;
+        String jsonArrayId = (String) request.getParameter("idForDelete");
+        /**
+         * @param gson.fromJson(String json, Object.class)
+         * Transforma el String tomado en un objeto List
+         */
+        List<String> IDs = (List<String>) gson.fromJson(jsonArrayId, Object.class);
+        
+        for (String id : IDs) {
+            
+            Long longID = Long.parseLong(id);
+            
+            try {
+                control.eliminarEmpleado(longID);
+                
+            } catch (Exception e) {
+                numNotDel += 1;
+                System.out.println(e.getMessage());
+            }
+            
+        }
+        
+        HttpSession notDelete = request.getSession();
+        notDelete.setAttribute("numNoEliminados", numNotDel);
+        
+        HttpSession arrIDsLength = request.getSession();
+        arrIDsLength.setAttribute("arrIDsForDelLength", IDs.size());
+        
+        response.sendRedirect("SvGoToEmployesList");
+    }
+    
+}
