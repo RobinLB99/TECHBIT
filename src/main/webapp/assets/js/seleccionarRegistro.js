a523123bb800bf14a9a1dd5210fc1f2cbec26cb7
RobinLB99 <rjlb991102@hotmail.com>
2024-04-01 23:01:43 -0500

    Comportamiento de botones de manipulación de registros

--- src/main/webapp/assets/js/seleccionarRegistro.js
+++ src/main/webapp/assets/js/seleccionarRegistro.js
@@ -1,12 +1,60 @@
 document.addEventListener("DOMContentLoaded", function() {
-	//Ingesar datos de IDs de registros a eliminar.
+	const btnBorrar = document.querySelector("#btnBorrarRegistro");
+	const btnActualizar = document.querySelector("#btnActualizarRegistro");
 	const formBorrarEmpleados = document.querySelector(
 		"#borrarRegistrosEmpleados"
 	);
+	const formModificarEmpleado = document.querySelector("#employModForm");
 	const checkboxes = document.getElementsByName("checkbox");
-	const inputIdForDelete = document.querySelector("#idForDelete");
+    const inputIdForDelete = document.querySelector("#idForDelete");
+    const inputIdForModify = document.querySelector("#inIdEmMod");
 	const arrayIdForDelete = [];
 
+	// Disponibilidad del boton borrar registro según los registros marcados
+	for (let i = 0; i < checkboxes.length; i++) {
+		checkboxes[i].addEventListener("change", function() {
+			for (let i = 0; i < checkboxes.length; i++) {
+				if (checkboxes[i].checked) {
+					btnBorrar.disabled = false;
+					btnBorrar.classList.remove("btn-secondary");
+					btnBorrar.classList.add("btn-danger");
+
+					return;
+				}
+			}
+
+			btnBorrar.disabled = true;
+			btnBorrar.classList.remove("btn-danger");
+			btnBorrar.classList.add("btn-secondary");
+		});
+	}
+
+	// Disponibilidad del boton Actualizar registro según los registros marcados
+	for (let i = 0; i < checkboxes.length; i++) {
+		checkboxes[i].addEventListener("change", function() {
+			let checkedCount = 0;
+
+			for (let i = 0; i < checkboxes.length; i++) {
+				if (checkboxes[i].checked) {
+					checkedCount++;
+				}
+			}
+
+			btnActualizar.disabled = checkedCount !== 1;
+
+			if (btnActualizar.disabled) {
+				btnActualizar.classList.remove("btn-warning");
+				btnActualizar.classList.add("btn-secondary");
+
+				return;
+			}
+
+			btnActualizar.classList.remove("btn-secondary");
+			btnActualizar.classList.add("btn-warning");
+		});
+	}
+
+	// Ingesar datos de IDs de registros a eliminar.
 	formBorrarEmpleados.addEventListener("submit", function(event) {
 		event.preventDefault();
 
@@ -22,5 +70,16 @@ document.addEventListener("DOMContentLoaded", function() {
 		this.submit();
 	});
 
-	//Ingesar datos de IDs de registros a eliminar.
+	// Ingesar ID de registro a modificar.
+	formModificarEmpleado.addEventListener("submit", function(event) {
+        event.preventDefault();
+        
+        checkboxes.forEach(elemento => {
+			if (elemento.checked) {
+                inputIdForModify.value = elemento.value;
+			}
+        });
+        
+        this.submit();
+	});
 });
