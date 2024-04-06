f0ae1f78e6df47305bb6162176c4c9d51886c5d8
RobinLB99 <rjlb991102@hotmail.com>
2024-04-05 20:54:47 -0500

    Se cambia el metodo de cierre de sesión acorde al AuthenticationFilter

---
+++ src/main/java/com/robinlb/techbit/servelets/auth/SvLogout.java
@@ -0,0 +1,34 @@
+package com.robinlb.techbit.servelets.auth;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ *
+ * Servicio de cierre de sesión.
+ */
+@WebServlet(name = "SvLogout", urlPatterns = {"/SvLogout"})
+public class SvLogout extends HttpServlet {
+
+  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    processRequest(request, response);
+
+    response.sendRedirect("Login.jsp");
+
+    HttpSession SessionActive = request.getSession(false);
+    SessionActive.invalidate();
+
+  }
+
+}
