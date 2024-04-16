633aa846a9cb284e82009d62da94491e852a0bf0
RobinLB99 <rjlb991102@hotmail.com>
2024-04-16 16:29:45 -0500

    Se cambia la forma en la que se accede a los recuros según este la sesion activa o no. Esto corrige el error de acceso a recursos para los ususarios colaboradores.

---
+++ src/main/java/com/robinlb/techbit/AuthenticationFilterColaborador.java
@@ -0,0 +1,63 @@
+package com.robinlb.techbit;
+
+import java.io.IOException;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Filtro de autentificación que permite validar si existe una sesión activa
+ * para el acceso a los recursos.
+ *
+ */
+public class AuthenticationFilterColaborador implements Filter {
+
+  public void init(FilterConfig fConfig) throws ServletException {
+  }
+
+  /**
+   * El metodo doFilter, valida si existe una sesión activa. Redirege a Login en
+   * caso de no existir una. Si existe, da acceso a los recursos. Los Servelets
+   * estan escluidos de la validación.
+   *
+   * @param request
+   * @param response
+   * @param chain
+   * @throws IOException
+   * @throws ServletException
+   */
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    HttpServletRequest httpRequest = (HttpServletRequest) request;
+    HttpServletResponse httpResponse = (HttpServletResponse) response;
+    String requestURI = httpRequest.getRequestURI();
+
+    HttpSession session = httpRequest.getSession(false);
+
+    if (session == null || session.getAttribute("user") == null) { // Solo si la sesion es nula o el atributo user sea nulo;
+
+      if (requestURI.equals("/TechBit/Login.jsp")) {
+        chain.doFilter(request, response);
+      } else {
+        httpResponse.sendRedirect("Login.jsp");
+      }
+
+    } else { // En caso de que la sesion no sea nula o el atributo user no sea nulo
+      if (requestURI.equals("/TechBit/Login.jsp")) {
+        httpResponse.sendRedirect("Dashboard.jsp");
+      } else {
+        chain.doFilter(request, response);
+      }
+    }
+  }
+
+  public void destroy() {
+  }
+
+}
