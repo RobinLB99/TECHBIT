cf8619a2ac1e7a18493cd419b9e40284b99e9946
joel <joel@joel-HP-Compaq-6000-Pro-SFF-PC>
2024-05-04 17:14:56 -0500

    Servicio de filtro para los recursos del cliente

---
+++ src/main/java/com/robinlb/techbit/AuthenticationFilterServeletsForCliente.java
@@ -0,0 +1,61 @@
+package com.robinlb.techbit;
+
+import com.robinlb.techbit.model.Usuario;
+import java.io.IOException;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Filtro de autentificación que permite validar si existe una sesión activa
+ * para el acceso a los recursos del cliente.
+ *
+ */
+public class AuthenticationFilterServeletsForCliente implements Filter {
+
+  public void init(FilterConfig fConfig) throws ServletException {
+  }
+
+  /**
+   * El metodo doFilter, valida si existe una sesión activa. Redirege a Login en
+   * caso de no existir una. Si existe, da acceso a los recursos. Los Servelets
+   * estan escluidos de la validación.
+   *
+   * @param request
+   * @param response
+   * @param chain
+   * @throws IOException
+   * @throws ServletException
+   */
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    HttpServletRequest httpRequest = (HttpServletRequest) request;
+    HttpServletResponse httpResponse = (HttpServletResponse) response;
+    HttpSession session = httpRequest.getSession(false);
+
+    try {
+      Usuario usuario = (Usuario) session.getAttribute("user");
+
+      if (usuario.getPrivilegios().equals("Administrador") 
+              || usuario.getPrivilegios().equals("Estandar")) {
+        httpResponse.sendRedirect("C-Dashboard.jsp");
+      } else {
+        chain.doFilter(request, response);
+      }
+    }
+    catch (Exception e) {
+      httpResponse.sendRedirect("Login_Cliente.jsp");
+    }
+
+  }
+
+  public void destroy() {
+  }
+
+}
