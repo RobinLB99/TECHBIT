b11934ba2b0c76d3aa3b75140985ab8e3b7f15a1
RobinLB99 <RJLB991102@hotmail.com>
2024-07-02 15:01:40 -0500

    .

--- src/main/webapp/assets/js/confirmar-asignacion.js
+++ src/main/webapp/assets/js/confirmar-asignacion.js
@@ -1,12 +1,25 @@
-let formsasign = document.getElementsByName("asignarticketform");
-const asignarbtn = document.getElementById("btnasign");
+// FORM TICKET ASIGN
+const formAsignTech = document.getElementById("asignTicketMethod");
+const idTicketSelected = document.getElementById("ticketIDSelected");
+const btnAsignTech = document.getElementById("btnAsignTech");
 
-formsasign.forEach(form => {
-   form.addEventListener("submit", (event) => {
-      event.preventDefault();
-      
-      asignarbtn.addEventListener("click", () => {
-            form.submit(); 
-      });
-   });
-});
+const listTickets = document.getElementsByName("_idTicket");
+
+formAsignTech.addEventListener("submit", (event) => {
+   event.preventDefault();
+})
+
+listTickets.forEach(ticket => {
+   ticket.addEventListener("change", () => {
+      if (ticket.checked) {
+         // TOMAR EL ID DEL TICKET
+         idTicketSelected.value = ticket.value;
+         // ACTIVAR EL BOTON DE ASIGNAR
+         btnAsignTech.disabled = false;
+      }
+   })
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+   btnAsignTech.disabled = true;
+})
