61c6e0e1bcbc92dab51a917716f35515f978aaa4
RobinLB99 <RJLB991102@hotmail.com>
2024-07-02 14:57:18 -0500

    Se añade funcion de busqueda de usuarios tecnicos por cargo.

--- src/main/java/com/robinlb/techbit/persistence/EmpleadoJpaController.java
+++ src/main/java/com/robinlb/techbit/persistence/EmpleadoJpaController.java
@@ -1,6 +1,7 @@
 package com.robinlb.techbit.persistence;
 
 import com.robinlb.techbit.model.Empleado;
+import com.robinlb.techbit.model.Usuario;
 import com.robinlb.techbit.persistence.exceptions.NonexistentEntityException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -18,136 +19,128 @@ import java.util.List;
  */
 public class EmpleadoJpaController implements Serializable {
 
-  public EmpleadoJpaController(EntityManagerFactory emf) {
-    this.emf = emf;
-  }
-  private EntityManagerFactory emf = null;
-
-  public EntityManager getEntityManager() {
-    return emf.createEntityManager();
-  }
-
-  public void create(Empleado empleado) {
-    EntityManager em = null;
-    try {
-      em = getEntityManager();
-      em.getTransaction().begin();
-      em.persist(empleado);
-      em.getTransaction().commit();
+    public EmpleadoJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
     }
-    finally {
-      if (em != null) {
-        em.close();
-      }
-    }
-  }
+    private EntityManagerFactory emf = null;
 
-  public void edit(Empleado empleado) throws NonexistentEntityException, Exception {
-    EntityManager em = null;
-    try {
-      em = getEntityManager();
-      em.getTransaction().begin();
-      empleado = em.merge(empleado);
-      em.getTransaction().commit();
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
     }
-    catch (Exception ex) {
-      String msg = ex.getLocalizedMessage();
-      if (msg == null || msg.length() == 0) {
-        Long id = empleado.getId();
-        if (findEmpleado(id) == null) {
-          throw new NonexistentEntityException("The empleado with id " + id + " no longer exists.");
+
+    public void create(Empleado empleado) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(empleado);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
         }
-      }
-      throw ex;
     }
-    finally {
-      if (em != null) {
-        em.close();
-      }
-    }
-  }
 
-  public void destroy(Long id) throws NonexistentEntityException {
-    EntityManager em = null;
-    try {
-      em = getEntityManager();
-      em.getTransaction().begin();
-      Empleado empleado;
-      try {
-        empleado = em.getReference(Empleado.class, id);
-        empleado.getId();
-      }
-      catch (EntityNotFoundException enfe) {
-        throw new NonexistentEntityException("The empleado with id " + id + " no longer exists.", enfe);
-      }
-      em.remove(empleado);
-      em.getTransaction().commit();
+    public void edit(Empleado empleado) throws NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            empleado = em.merge(empleado);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Long id = empleado.getId();
+                if (findEmpleado(id) == null) {
+                    throw new NonexistentEntityException("The empleado with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
-    finally {
-      if (em != null) {
-        em.close();
-      }
-    }
-  }
 
-  public List<Empleado> findEmpleadoEntities() {
-    return findEmpleadoEntities(true, -1, -1);
-  }
+    public void destroy(Long id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Empleado empleado;
+            try {
+                empleado = em.getReference(Empleado.class, id);
+                empleado.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The empleado with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(empleado);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
 
-  public List<Empleado> findEmpleadoEntities(int maxResults, int firstResult) {
-    return findEmpleadoEntities(false, maxResults, firstResult);
-  }
+    public List<Empleado> findEmpleadoEntities() {
+        return findEmpleadoEntities(true, -1, -1);
+    }
 
-  private List<Empleado> findEmpleadoEntities(boolean all, int maxResults, int firstResult) {
-    EntityManager em = getEntityManager();
-    try {
-      CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
-      cq.select(cq.from(Empleado.class));
-      Query q = em.createQuery(cq);
-      if (!all) {
-        q.setMaxResults(maxResults);
-        q.setFirstResult(firstResult);
-      }
-      return q.getResultList();
+    public List<Empleado> findEmpleadoEntities(int maxResults, int firstResult) {
+        return findEmpleadoEntities(false, maxResults, firstResult);
     }
-    finally {
-      em.close();
-    }
-  }
 
-  public Empleado findEmpleado(Long id) {
-    EntityManager em = getEntityManager();
-    try {
-      return em.find(Empleado.class, id);
+    private List<Empleado> findEmpleadoEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Empleado.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
     }
-    finally {
-      em.close();
-    }
-  }
-  
-  public List<Empleado> findTecnicoSoportePorCargo(String cargo) {
-      EntityManager em = getEntityManager();
-      try {
-          Query query = em.createQuery("SELECT t FROM Empleado.t WHERE t.cargo = :cargo");
-          query.setParameter("cargo", cargo);
-          return (List<Empleado>) query.getResultList();
-      } finally {
-          em.close();
-      }
-  }
 
-  public int getEmpleadoCount() {
-    EntityManager em = getEntityManager();
-    try {
-      CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
-      Root<Empleado> rt = cq.from(Empleado.class);
-      cq.select(em.getCriteriaBuilder().count(rt));
-      Query q = em.createQuery(cq);
-      return ((Long) q.getSingleResult()).intValue();
+    public Empleado findEmpleado(Long id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Empleado.class, id);
+        } finally {
+            em.close();
+        }
     }
-    finally {
-      em.close();
+
+    public List<Empleado> findTecnicoSoportePorCargo(String _cargo) {
+        EntityManager em = getEntityManager();
+        try {
+            Query query = em.createQuery("SELECT t FROM Empleado t WHERE t.cargo = :cargo");
+            query.setParameter("cargo", _cargo);
+            return (List<Empleado>) query.getResultList();
+        } finally {
+            em.close();
+        }
     }
-  }
-  
+
+    public int getEmpleadoCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Empleado> rt = cq.from(Empleado.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+
 }
